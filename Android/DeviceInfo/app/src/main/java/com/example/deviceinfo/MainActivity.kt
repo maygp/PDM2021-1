7815f75622e74b1c9a7fe46dd5606e927ee31818
Mayara <mayaragomper@gmail.com>
2021-07-24 23:50:20 -0300

    adicionando novas informacoes

--- Android/DeviceInfo/app/src/main/java/com/example/deviceinfo/MainActivity.kt
+++ Android/DeviceInfo/app/src/main/java/com/example/deviceinfo/MainActivity.kt
@@ -9,22 +9,24 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvInfo : TextView
+    private lateinit var tvInfo2 : TextView
     private lateinit var btSobre : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.tvInfo = findViewById(R.id.tvInfo2)
+        this.tvInfo = findViewById(R.id.tvInfo)
+        this.tvInfo2 = findViewById(R.id.tvInfo2)
         this.btSobre = findViewById(R.id.btSobre)
 
         this.btSobre.setOnClickListener(OnClickBt())
-
     }
 
     inner class OnClickBt : View.OnClickListener {
         override fun onClick(v: View?){
-            this@MainActivity.tvInfo.text = Build.BRAND
+            this@MainActivity.tvInfo.text = "Marca do dispositivo: " + Build.BRAND
+            this@MainActivity.tvInfo2.text = "Build.BRAND exibe a marca à qual o produto está associado."
         }
     }
 }
